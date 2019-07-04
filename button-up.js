be1ef0dcf2433d84da53783f33034ea343a0f713
Beata Rudzka <beatarudzka89@gmail.com>
2019-07-04 16:57:02 +0200

    adding button up

--- button-up.js
+++ button-up.js
@@ -1,12 +1,25 @@
-$(window).scroll(function() {
-    if ($(this).scrollTop() >= 50) { // If page is scrolled more than 50px
-        $('#return-to-top').fadeIn(200); // Fade in the arrow
-    } else {
-        $('#return-to-top').fadeOut(200); // Else fade out the arrow
-    }
-});
-$('#return-to-top').click(function() { // When arrow is clicked
-    $('body,html').animate({
-        scrollTop: 0 // Scroll to top of body
-    }, 500);
+$(function() {
+    var isClicked = false;
+    $('.scrollTop').css("display", "none");
+    $(window).scroll(function() {
+        if (isClicked == false) {
+            if ($(this).scrollTop() > 300) { $('.scrollTop').show(); } else { $('.scrollTop').hide(); }
+        }
+    });
+
+    $('.scrollTop').click(function() {
+        isClicked = true;
+        $('.scrollTop').fadeOut(500);
+        $('html, body').animate({
+            scrollTop: 0
+        }, 800, function() {
+            isClicked = false;
+        });
+    });
+    $('.scrollTop').on("mouseenter", function() {
+        $('.scrollTop').css({ opacity: '1.0' });
+    });
+    $('.scrollTop').on("mouseleave", function() {
+        $('.scrollTop').css({ opacity: '0.5' });
+    });
 });
