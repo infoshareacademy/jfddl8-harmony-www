51a5da52b03aaf228428626988b76dc2efe08f03
Angelika Maciazek <maciazekangelika@gmail.com>
2019-07-11 15:43:10 +0200

    wip animation  test

--- test.js
+++ test.js
@@ -1,12 +1,13 @@
-console.log("tutut");
 $(document).ready(function() {
   $(window).scroll(function() {
     const scroll = $(window).scrollTop();
+    const imgAuthor = $(".author__img")
+    console.log('img author:', imgAuthor)
     console.log(scroll);
-    if (scroll >= 1150) {
-      $(".authors__author").addClass("slide-top");
+    if (scroll >= 1100) {
+      imgAuthor.addClass("slide-fwd-center");
     } else {
-      $(".authors__author").removeClass("slide-top");
+      imgAuthor.removeClass("slide-fwd-center");
     }
   });
 });
