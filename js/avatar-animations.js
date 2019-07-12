f26f2d524cdcb175d8c353d7a9a9d7de8b40d07f
Henryk Krzyszczak <henrykkrzyszczak@gmail.com>
2019-07-12 16:04:28 +0200

    add alternative cooki

--- js/avatar-animations.js
+++ js/avatar-animations.js
@@ -2,10 +2,8 @@ $(window).scroll(function() {
   const position = $("#am").offset();
   const scroll = $(window).scrollTop() + $(window).height();
   const avatarHeight = $("#am").height();
-  console.log(scroll, position);
 
   if (scroll >= position.top + avatarHeight) {
-    console.log("animation start");
     setTimeout(animate, 1000);
     function animate() {
       $("#am").addClass("rotate");
