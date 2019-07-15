7e4139ef39d87f2a3e86a8498b3f3f9e3f69f0d3
AdamWitzb <a.witzberg@gmail.com>
2019-07-15 16:41:53 +0200

    menu styled with collapse effect

---
+++ header-scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+  let scrollTop = 0;
+  $(window).scroll(function(){
+    scrollTop = $(window).scrollTop();
+     $('.counter').html(scrollTop);
+    
+    if (scrollTop >= 100) {
+      $('#global-nav').addClass('scrolled-nav');
+    } else if (scrollTop < 100) {
+      $('#global-nav').removeClass('scrolled-nav');
+    } 
+    
+  }); 
+  
+});
