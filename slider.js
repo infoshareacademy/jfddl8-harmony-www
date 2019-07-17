e31ab62a553e9d60dca912d482b84859b1d79fe7
Beata Rudzka <beatarudzka89@gmail.com>
2019-07-17 19:52:27 +0200

    adding photos

--- slider.js
+++ slider.js
@@ -1,47 +1,109 @@
-jQuery(document).ready(function($) {
-    let slideCount = $('.sliderblock__list--pict').length
-    let slideWidth = $('.sliderblock__list--pict').width
-    let slideHeight = $('.sliderblock__list--pict').height
-    let sliderUlWidth = slideCount * slideWidth
+// SLIDER
+const TIMENEXTSLIDE = 3000;
+const SLIDETIME = 1000;​
+const slides = document.querySelectorAll('.carousel__slide');
+let prevSlideIndex = slides.length - 1;
+let currentSlideIndex = 0;​
+let currentIntervalId = null;
+let canIClick = true;​
+const clearSlides = () => {
+    slides.forEach(
+        slide => {
+            slide.classList.remove('carousel__slide--active');
+            slide.classList.remove('carousel__slide--preserved');
+        }
+    );
+};​
+const updateDisplay = () => {
+    const prevSlide = slides[prevSlideIndex];
+    const currentSlide = slides[currentSlideIndex];​
+    clearSlides();​
+    preserveSlide(prevSlide);
+    makeSlideActive(currentSlide);
+};​
+const preserveSlide = (slide) => {
+    slide.classList.add('carousel__slide--preserved');
+    setTimeout(
+        () => {
+            slide.classList.remove('carousel__slide--preserved');
+        },
+        TIMENEXTSLIDE
+    );
+};​
+const makeSlideActive = (slide) => {
+    slide.classList.add('carousel__slide--active');
+};​
+const startInterval = () => {
+    currentIntervalId = setInterval(
+        () => {
+            nextSlide();
+        },
+        TIMENEXTSLIDE
+    );
+};​
+const nextSlide = () => {
+    prevSlideIndex = currentSlideIndex;​
+    const nextSlide = currentSlideIndex + 1;
+    if (nextSlide > slides.length - 1) {
+        currentSlideIndex = 0;
+    } else {
+        currentSlideIndex = nextSlide;
+    }​
+    updateDisplay();
+};​
+const prevSlide = () => {
+    prevSlideIndex = currentSlideIndex;
+    const prevSlide = currentSlideIndex - 1;
+    if (prevSlide < 0) {
+        currentSlideIndex = slides.length - 1;
+    } else {
+        currentSlideIndex = prevSlide;
+    }​
+    updateDisplay();
+};​
+// CONTROLS
+​
+document.querySelector('.prev').addEventListener("click", () => {
+    if (canIClick) {
 
-    $('.sliderblock--slider').css({
-        width: slideWidth,
-        height: slideHeight
-    })
-    $('.sliderblock__list').css({
-        width: sliderUlWidth,
-        marginLeft: -slideWidth
-    })
-    $('.sliderblock__list--pict:last-child').prependTo('.sliderblock__list')
-
-    function moveLeft() {
-        $('.sliderblock__list').animate({
-            left: +slideWidth
-        }, 200, function() {
-            $('.sliderblock__list--pict:last-child').prependTo('.sliderblock__list')
-            $('.sliderblock__list').css('left', '')
-        })
+        prevSlide();
+        canIClick = false;
+        setTimeout(() => canIClick = true, SLIDETIME);
     }
-
-    function moveRight() {
-        $('.sliderblock__list').animate({
-            left: -slideWidth
-        }, 200, function() {
-            $('.sliderblock__list--pict:first-child').appendTo('.sliderblock__list')
-            $('.sliderblock__list').css('left', '')
-        })
+});​
+document.querySelector('.next').addEventListener("click", () => {
+    if (canIClick) {
+        nextSlide();
+        canIClick = false;
+        setTimeout(() => canIClick = true, SLIDETIME);
     }
-
-
-    $('.sliderblock--prevarrow').click(function() {
-        moveLeft()
-    })
-    $('.sliderblock--nextarrow').click(function() {
-        moveRight()
-    })
-
-
-    setInterval(function() {
-        moveRight()
-    }, 3000)
-})
+});​
+document.querySelector("slider__carousel-wrapper").addEventListener("mouseenter", () => {
+    clearInterval(currentIntervalId);
+    currentIntervalId = null;
+});​
+document.querySelector(".slider__carousel-wrapper").addEventListener("mouseleave", () => {
+    if (!currentIntervalId) {
+        startInterval();
+    }
+});​
+// AUTO SLIDES
+​
+startInterval();​​​​
+// ANIMATE PHOTOS
+const teammatesContainer = document.querySelector('.teammates');
+const teammatesPhotos = document.querySelectorAll('.teammate-item__photo');
+const teammatesContainerHeight = teammatesContainer.clientHeight;​
+function isElementInView() {
+    const windowHeight = window.innerHeight;
+    const scrollY = window.scrollY || window.pageYOffset;
+    const scrollPosition = scrollY + windowHeight;
+    const elementPosition = teammatesContainer.getBoundingClientRect().top + scrollY + teammatesContainerHeight;
+    return scrollPosition > elementPosition;
+}​
+function animate() {
+    if (isElementInView()) {
+        teammatesPhotos.forEach(photo => photo.classList.add('teammates-animate'));
+    }
+}​
+document.addEventListener('scroll', animate);
