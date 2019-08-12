86f18fcfb73973737a4ade4eab0ffff621737a63
Angelika <maciazekangelika@gmail.com>
2019-08-12 02:57:54 +0200

    create the bricks

--- game/game.js
+++ game/game.js
@@ -126,4 +126,33 @@ function ballPaddleCollision(){
     }
 }
 
+const brick = {
+    row : 1,
+    column : 5,
+    width : 55,
+    height : 20,
+    offSetLeft : 20,
+    offSetTop : 20,
+    marginTop : 40,
+    fillColor : "#2e3548",
+    strokeColor : "#FFF"
+}
+
+let bricks = []
+
+function createBricks(){
+    for(let r = 0; r < brick.row; r++){
+        bricks[r] = []
+        for(let c = 0; c < brick.column; c++){
+            bricks[r][c] = {
+                x : c * ( brick.offSetLeft + brick.width ) + brick.offSetLeft,
+                y : r * ( brick.offSetTop + brick.height ) + brick.offSetTop + brick.marginTop,
+                status : true
+            }
+        }
+    }
+}
+
+createBricks()
+
 
